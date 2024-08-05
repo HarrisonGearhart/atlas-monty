669165873a67109e39a755d22d963d3977aa83f0
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-08-05 13:51:19 -0500

    nop.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * nop - nothing
+ * @head: stack head
+ * @line_count: line counter
+ * Return: Void
+*/
+void _nop(stack_t **head, unsigned int line_count)
+{
+	(void) head;
+	(void) line_count;
+}
