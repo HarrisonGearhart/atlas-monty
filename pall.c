810d7f9a4757eba32f94b74a6f3bb9671d6e4651
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-08-05 12:10:29 -0500

    pall.c/ create

---
+++ pall.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * _pall - prints everything in the stack
+ * @head: double pointer to first node
+ * @line_count: line counter(not used)
+ * Return: none
+*/
+void _pall(stack_t **head, unsigned int line_count)
+{
+	stack_t *h;
+	(void)line_count;
+
+	h = *head;
+	if (h = NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
